90caafc5ea2632ff0626d2197b1e2d09e6e4bf16
GluKosAlex <glukosalex@gmail.com>
2024-02-12 22:39:13 +0700

    build(app): initial commit environment setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <main>
+      <div className='p-4'>
+        <Button variant='secondary'> click </Button>
+      </div>
+    </main>
+  );
+}
