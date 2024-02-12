f14992979659190d497ed469f208d84c3682d3aa
GluKosAlex <glukosalex@gmail.com>
2024-02-13 01:48:53 +0700

    feat(app): add shadcn dialog component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import { UserButton } from '@clerk/nextjs';
+import Modal from '@/components/ui/modal';
 
 export default function RootPage() {
   return (
     <main>
       <div className='p-4'>
-        <UserButton afterSignOutUrl='/'/>
+        <Modal isOpen={true} description='test' title='test' onClose={() => {}}>
+          Children
+        </Modal>
       </div>
     </main>
   );
---
+++ components/ui/modal.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode } from 'react';
+
+import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogHeader } from '@/components/ui/dialog';
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal: FC<ModalProps> = ({ title, description, isOpen, onClose, children }) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+          <div>{children}</div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Modal;
